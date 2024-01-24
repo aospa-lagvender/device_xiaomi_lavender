47ce802648113c48367d724cf74bf7b438f6b681
me-cafebabe <me.cafebabe@gmail.com>
2024-01-24 19:07:19 -0300

    lavender: libstdc++.vendor => libstdc++_vendor
    Change-Id: I73f0d1bf59128c1b3ff2df16f22d0f9a5f1f0365
    Signed-off-by: SH4ND <miqueiasantos20@gmail.com>

--- device.mk
+++ device.mk
@@ -66,7 +66,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl:32 \
     android.hardware.camera.provider@2.4-service \
     libdng_sdk.vendor \
-    libstdc++.vendor \
+    libstdc++_vendor \
     vendor.qti.hardware.camera.device@1.0:64
 
 PRODUCT_COPY_FILES += \
